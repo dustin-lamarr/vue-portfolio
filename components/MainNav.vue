a0c57bcbc0c2c2ae1b64fa14b0b9d3ec4249a7d1
Dustin LaMarr <dustin.lamarr@gmail.com>
2023-02-25 08:30:51 -0700

    Decided to change design of main nav
    moved image into li element and centered

--- components/MainNav.vue
+++ components/MainNav.vue
@@ -3,19 +3,19 @@
     class="flex flex-row bg-black fixed top-0 left-0 border-b-4 border-punkash-dark w-screen items-end text-punkash tracking-tight text-2xl"
   >
     <ul
-      class="text-sm flex space-x-4 text-center"
+      class="text-sm flex space-x-4 text-center justify-center items-end w-full"
       :class="{ 'text-xl space-x-10 ml-10 mb-4': screen }"
     >
-      <li>Projects</li>
-      <li>Mission</li>
-      <li>TBD</li>
+      <li class="flex">Projects</li>
+      <li>
+        <img
+          :class="{ 'w-32 mr-0': screen }"
+          class="h-auto w-20 ml-auto my-3"
+          src="../assets/punkash_bee_outline.png"
+        />
+      </li>
+      <li class="flex">Mission</li>
     </ul>
-
-    <img
-      :class="{ 'w-32 mr-0': screen }"
-      class="h-auto w-20 ml-auto my-3"
-      src="../assets/punkash_bee_outline.png"
-    />
   </nav>
 </template>
 <script>
