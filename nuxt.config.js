0ef14812a9b670293a367bdc8728abf7cc5c6df2
Dustin LaMarr <dustin.lamarr@gmail.com>
2023-03-19 15:11:10 -0600

    Updated favicon hopefully. Adjusted homie image size

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  head: { link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.png" }] },
   modules: ["@nuxtjs/tailwindcss"],
 });
